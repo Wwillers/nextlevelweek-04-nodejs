1c6bcb38433899a7904775b75e1d428edee97184
William Willers <w.willers@outlook.com>
2021-02-24 21:21:21 -0400

    Criando Controller, Migration e Model de Survey

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -3,27 +3,25 @@ import { getCustomRepository } from 'typeorm';
 import { UsersRepository } from '../repositories/UsersRepository';
 
 class UserController {
+  async create(req: Request, res: Response) {
+    const { name, email } = req.body;
 
-    async create(req: Request, res: Response) {
-        const { name, email } = req.body;
-        
-        const usersRepository = getCustomRepository(UsersRepository);
-        const userAlreadyExists = await usersRepository.findOne({
-            email
-        });
+    const usersRepository = getCustomRepository(UsersRepository);
+    const userAlreadyExists = await usersRepository.findOne({
+      email,
+    });
 
-        if(userAlreadyExists) {
-            return res.status(400).json({
-                error: "User already exists!",
-            });
-        }
-
-        const user = usersRepository.create({ name, email })
-        await usersRepository.save(user)
-
-        return res.json(user);
+    if (userAlreadyExists) {
+      return res.status(400).json({
+        error: 'User already exists!',
+      });
     }
 
+    const user = usersRepository.create({ name, email });
+    await usersRepository.save(user);
+
+    return res.json(user);
+  }
 }
 
 export { UserController };

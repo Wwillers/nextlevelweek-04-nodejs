db96fe2ab9a08127b5298a97aa95c99e6dc1cab3
William Willers <w.willers@outlook.com>
2021-02-26 14:27:00 -0400

    Criando AnswerController

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { AnswerController } from './controllers/AnswerController';
 import { SendMailController } from './controllers/SendMailController';
 import { SurveyController } from './controllers/SurveyController';
 import { UserController } from './controllers/UserController';
@@ -8,6 +9,7 @@ const router = Router();
 const userController = new UserController();
 const surveyController = new SurveyController();
 const sendMail = new SendMailController();
+const answerController = new AnswerController();
 
 //Users
 router.post('/users', userController.create);
@@ -18,5 +20,8 @@ router.get('/surveys', surveyController.show);
 
 //Mail
 router.post('/sendMail', sendMail.execute);
+
+//Answers
+router.get('/answers/:value', answerController.execute);
 
 export { router };

6c546373c302c0cbe6080c1ad5a3d6be8841ea45
William Willers <w.willers@outlook.com>
2021-02-26 19:31:01 -0400

    Tratamento de erros

--- src/controllers/AnswerController.ts
+++ src/controllers/AnswerController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
+import { AppError } from '../errors/AppError';
 import { SurveysUsersRepository } from '../repositories/SurveysUsersRepository';
 
 class AnswerController {
@@ -14,9 +15,7 @@ class AnswerController {
     });
 
     if (!surveyUser) {
-      return res.status(400).json({
-        error: 'Survey user does not exist',
-      });
+      throw new AppError('Survey user does not exist');
     }
 
     surveyUser.value = Number(value);
--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,6 +2,7 @@ import { Request, response, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
 import { UsersRepository } from '../repositories/UsersRepository';
 import * as yup from 'yup';
+import { AppError } from '../errors/AppError';
 
 class UserController {
   async create(req: Request, res: Response) {
@@ -15,7 +16,7 @@ class UserController {
     try {
       await schema.validate(req.body, { abortEarly: false });
     } catch (err) {
-      return res.status(400).json({ error: err });
+      throw new AppError(err);
     }
 
     const usersRepository = getCustomRepository(UsersRepository);
@@ -24,9 +25,7 @@ class UserController {
     });
 
     if (userAlreadyExists) {
-      return res.status(400).json({
-        error: 'User already exists!',
-      });
+      throw new AppError('User already exists!');
     }
 
     const user = usersRepository.create({ name, email });
